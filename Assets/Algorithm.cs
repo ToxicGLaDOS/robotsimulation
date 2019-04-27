574aa4dc1f175544a482b13f99901e9ca8dcbf49
Jeff Smith <smit8397@d.umn.edu>
2019-04-27 16:11:14 -0500

    Added logic to figure out starting position

--- Assets/Algorithm.cs
+++ Assets/Algorithm.cs
@@ -7,40 +7,71 @@ public class Algorithm : MonoBehaviour
     Movement movement;
     Sensors sensors;
     public GridMap grid;
+    public WallGenerator wallGen;
+    float xmin, ymin, xmax, ymax; 
 
 
-
+       
     public int turnAmount = 0;
 
-    Vector2 gridPos = new Vector2(50,0);
+    public float gridPixelWidth, gridPixelHeight;
+    public Vector2Int gridPos;
     int turnCounter = 0;
     int turnTo = 1;
+    float error = 0;
     // Start is called before the first frame update
     void Start()
     {
         movement = GetComponent<Movement>();
         sensors = GetComponent<Sensors>();
+
+        xmin = wallGen.xmin;
+        ymin = -5;
+        xmax = wallGen.xmax;
+        ymax = 5;
+
+        gridPixelWidth = (xmax - xmin) / grid.width;
+        gridPixelHeight = (ymax - ymin) / grid.height;
+
+        int y = Mathf.RoundToInt((ymax - transform.position.y) / gridPixelHeight);
+
+        gridPos = new Vector2Int(grid.width / 2, y);
     }
 
     void DrawGrid(float[] readings) {
         int x1 = Mathf.RoundToInt(gridPos.x);
         int y1 = Mathf.RoundToInt(gridPos.y);
-        int x2 = Mathf.RoundToInt(gridPos.x);
-        int y2 = Mathf.RoundToInt(gridPos.y + readings[4]*30);
-        print(y1);
-        print(y2);
-        List<Vector2> points = GridMap.bresenham(x1, y1, x2, y2);
-        Vector2 lastPoint = points[points.Count-1];
-
         
+        for (int i = 0; i < readings.Length; i++)
+        {
+            float angle = (2 * Mathf.PI / 16) * i;
 
-        foreach (Vector2 point in points) {
-            grid.SetPixel(Mathf.RoundToInt(point.x), Mathf.RoundToInt(point.y), System.Drawing.Color.White);
+            int x2 = Mathf.RoundToInt(x1 + Mathf.Cos(angle) * (readings[i] / gridPixelWidth));
+            int y2 = Mathf.RoundToInt(y1 - Mathf.Sin(angle) * (readings[i] / gridPixelHeight));
+
+            List<Vector2Int> points = GridMap.bresenham(x1, y1, x2, y2);
+            Vector2Int lastPoint = points[points.Count - 1];
+
+
+
+            foreach (Vector2Int point in points)
+            {
+                grid.SetPixel(point.x, point.y, System.Drawing.Color.White);
+            }
+
+            grid.SetPixel(lastPoint.x, lastPoint.y, System.Drawing.Color.Black);
         }
 
-        grid.SetPixel(Mathf.RoundToInt(lastPoint.x), Mathf.RoundToInt(lastPoint.y), System.Drawing.Color.Black);
+    }
 
-
+    void Move(float forward, float turn) {
+        movement.PutMovement(forward, turn);
+        error += movement.speed * forward / gridPixelHeight;
+        while (error > 1)
+        {
+            gridPos += new Vector2Int(0, -1);
+            error--;
+        }
     }
 
     // turnTo 1 means left turnTo -1 means right.
--- Assets/GridMap.cs
+++ Assets/GridMap.cs
@@ -41,9 +41,50 @@ public class GridMap : MonoBehaviour
     }
 
     public void SetPixel(int x, int y, System.Drawing.Color color) {
-        image.SetPixel(x, y, color);
-    }
+        if (x > 0 && x < width && y > 0 && y < height)
+        {
+            image.SetPixel(x, y, color);
+        }
+    } 
+    public static List<Vector2Int> bresenham(int x, int y, int x2, int y2)
+    {
+        List<Vector2Int> points = new List<Vector2Int>();
+        int w = x2 - x;
+        int h = y2 - y;
+        int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+        if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+        if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+        if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+        int longest = Mathf.Abs(w);
+        int shortest = Mathf.Abs(h);
+        if (!(longest > shortest))
+        {
+            longest = Mathf.Abs(h);
+            shortest = Mathf.Abs(w);
+            if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+            dx2 = 0;
+        }
+        int numerator = longest >> 1;
+        for (int i = 0; i <= longest; i++)
+        {
+            points.Add(new Vector2Int(x, y));
+            numerator += shortest;
+            if (!(numerator < longest))
+            {
+                numerator -= longest;
+                x += dx1;
+                y += dy1;
+            }
+            else
+            {
+                x += dx2;
+                y += dy2;
+            }
+        }
 
+        return points;
+    }
+    /*
     public static List<Vector2> bresenham(int x1, int y1, int x2, int y2)
     {
         List<Vector2> points = new List<Vector2>();
@@ -70,4 +111,5 @@ public class GridMap : MonoBehaviour
 
         return points;
     }
+    */
 }
